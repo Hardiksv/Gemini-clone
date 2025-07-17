60b29855004b4a951433d928ac0dfed32b44d49c
Hardik S V <135291597+Hardiksv@users.noreply.github.com>
2025-07-17 08:48:37 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,60 +1,10 @@
-// const apikey ="AIzaSyCLGPWORojdUGZacnWLl0AQQ-nVh9ecBKg";
 
-// To run this code you need to install the following dependencies:
-// npm install @google/genai mime
-// npm install -D @types/node
 
 import {
   GoogleGenAI,
 } from '@google/genai';
 
-// async function main(prompt) {
-//   const ai = new GoogleGenAI({
-//     apiKey: 'AIzaSyCLGPWORojdUGZacnWLl0AQQ-nVh9ecBKg',
-//   });
-//   const tools = [
-//     {
-//       googleSearch: {
-//       }
-//     },
-//   ];
-//   const config = {
-//     thinkingConfig: {
-//       thinkingBudget: -1,
-//     },
-//     tools,
-//     responseMimeType: 'text/plain',
-//   };
-//   const model = 'gemini-2.5-pro';
-//   const contents = [
-//     {
-//       role: 'user',
-//       parts: [
-//         {
-//           text: prompt,
-//         },
-//       ],
-//     },
-//   ];
 
-//   const response = await ai.models.generateContentStream({
-//     model,
-//     config,
-//     contents,
-//   });
-
-//   let finalResponse = '';
-//   for await (const chunk of response) {
-//     if (chunk.text) {
-//       console.log(chunk.text);        
-//       finalResponse += chunk.text;    
-//     }
-//   }
-
-//   return finalResponse;  
-// }
-
-// export default main;
 
 async function main(prompt) {
   const response = await fetch("https://gemini-clone-m3rr.onrender.com/api/gemini", {
